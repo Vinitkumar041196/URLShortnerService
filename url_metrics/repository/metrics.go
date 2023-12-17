249c4136b64abd0756cf9ed867967b20852fe438
Vinitkumar041196 <vksg041196@gmail.com>
2023-12-17 16:13:53 +0530

    metrics repo implemented

---
+++ url_metrics/repository/metrics.go
@@ -0,0 +1,80 @@
+package repository
+
+import (
+	"fmt"
+	"sort"
+	"sync"
+)
+
+type Metric struct {
+	Domain string
+	Count  int
+}
+
+type inMemoryMetricStore struct {
+	countMetricStore map[string]Metric
+	lock             sync.Mutex
+}
+
+func NewInMemoryURLStore() *inMemoryMetricStore {
+	return &inMemoryMetricStore{
+		countMetricStore: make(map[string]Metric),
+	}
+}
+
+func (store *inMemoryMetricStore) IncreementDomainShortenCount(domain string) error {
+	//check if store exists
+	if store.countMetricStore == nil {
+		return fmt.Errorf("store not initialized")
+	}
+
+	//acquire a lock on map to avoid simultaneous read write
+	store.lock.Lock()
+	//release lock on return
+	defer store.lock.Unlock()
+
+	if metric, ok := store.countMetricStore[domain]; !ok {
+		//if not found then set the count to 1
+		store.countMetricStore[domain] = Metric{Domain: domain, Count: 1}
+	} else {
+		//if found then increement the count
+		metric.Count += 1
+		store.countMetricStore[domain] = metric
+	}
+	return nil
+}
+
+func (store *inMemoryMetricStore) GetTopDomains(limit int) (map[string]int, error) {
+	//check if store exists
+	if store.countMetricStore == nil {
+		return nil, fmt.Errorf("store not initialized")
+	}
+
+	//acquire a lock on map to avoid simultaneous read write
+	store.lock.Lock()
+
+	//get the values in array
+	metricArray := []Metric{}
+	for _, metric := range store.countMetricStore {
+		metricArray = append(metricArray, metric)
+	}
+
+	//release lock
+	store.lock.Unlock()
+
+	//sort the metric array
+	sort.Slice(metricArray, func(i, j int) bool {
+		return metricArray[i].Count > metricArray[j].Count
+	})
+
+	topNDomainMap := make(map[string]int)
+
+	if limit > len(metricArray) {
+		limit = len(metricArray)
+	}
+
+	for _, metric := range metricArray[:limit] {
+		topNDomainMap[metric.Domain] = metric.Count
+	}
+	return topNDomainMap, nil
+}
